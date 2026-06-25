4d1058aceb917dec7bee668eacc1b8f834e32d49
gzordrai <gzordrai@proton.me>
2026-06-25 15:16:17 +0200

    feat: implement jobs submit

--- src/error.rs
+++ src/error.rs
@@ -3,4 +3,19 @@ use thiserror::Error;
 pub type Result<T> = core::result::Result<T, MangonneauError>;
 
 #[derive(Debug, Error)]
-pub enum MangonneauError {}
+pub enum MangonneauError {
+    #[error("missing or invalid environment variable {0}")]
+    EnvVar(#[from] std::env::VarError),
+
+    #[error("failed to parse TOML config: {0}")]
+    TomlParse(#[from] toml::de::Error),
+
+    #[error("failed to serialize TOML: {0}")]
+    TomlSerialize(#[from] toml::ser::Error),
+
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("G5K client error: {0}")]
+    G5KClient(#[from] g5k::error::G5KError),
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,88 @@
+use std::{fs, path::PathBuf};
+
+use clap::Parser;
+use futures::{StreamExt, TryStreamExt, stream};
+use g5k::{G5KClient, Site, job::JobPayload};
+use serde::Serialize;
+
+use crate::{
+    cli::Args,
+    config::Config,
+    error::{MangonneauError, Result},
+};
+
+mod cli;
+mod config;
 mod error;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Default, Serialize)]
+pub struct BenchJobs<'a> {
+    jobs: Vec<BenchJob<'a>>,
+}
+
+impl<'a> BenchJobs<'a> {
+    pub fn new(jobs: Vec<BenchJob<'a>>) -> Self {
+        Self { jobs }
+    }
+}
+
+#[derive(Serialize)]
+pub struct BenchJob<'a> {
+    id: String,
+    site: Site,
+    node: &'a str,
+    cpu: &'a str,
+}
+
+impl<'a> BenchJob<'a> {
+    pub fn new(id: String, site: Site, node: &'a str, cpu: &'a str) -> Self {
+        Self {
+            id,
+            site,
+            node,
+            cpu,
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    let data = std::fs::read(args.file)?;
+    let config: Config = toml::from_slice(&data)?;
+    let client = G5KClient::new(args.username, args.password);
+
+    let jobs: Vec<BenchJob> = stream::iter(config.bench.into_iter())
+        .map(|b| {
+            let client = &client;
+            let command = &args.command;
+            let directory = args.directory.as_deref();
+
+            async move {
+                let res = client
+                    .submit_job(
+                        b.site,
+                        &JobPayload {
+                            command,
+                            queue: Some(b.queue),
+                            properties: Some(b.node),
+                            directory,
+                            ..Default::default()
+                        },
+                    )
+                    .await?;
+
+                Ok::<_, MangonneauError>(BenchJob::new(res, b.site, b.node, b.cpu))
+            }
+        })
+        .buffer_unordered(4)
+        .try_collect()
+        .await?;
+
+    fs::write(
+        args.output.unwrap_or(PathBuf::from("./output.toml")),
+        toml::to_string_pretty(&BenchJobs::new(jobs))?,
+    )?;
+
+    Ok(())
 }
